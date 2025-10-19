6bf2c6b2d156296b4b0d943d6eadb604ae82d87c
Aron <winkler.aron5@gmail.com>
2025-10-19 11:47:25 +0200

    claude

--- lua/theprimeagen/lazy/codesnap.lua
+++ lua/theprimeagen/lazy/codesnap.lua
@@ -2,8 +2,7 @@ return {
 	"mistricky/codesnap.nvim",
 	build = "make",
 	keys = {
-		{ "<leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
-		{ "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+		{ "<leader>cs", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
 	},
 	opts = {
 		save_path = "~/Pictures",
