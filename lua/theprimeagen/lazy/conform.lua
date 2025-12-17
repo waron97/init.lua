6b2235bedabb82bcbd8a3361f2d491f6b62bf5ab
Aron <winkler.aron5@gmail.com>
2025-12-17 14:49:10 +0100

    formatter timeout

--- lua/theprimeagen/lazy/conform.lua
+++ lua/theprimeagen/lazy/conform.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		require("conform").setup({
 			format_on_save = {
-				timeout_ms = 0,
+				timeout_ms = 5000,
 				lsp_format = "fallback",
 				pattern = "*", -- format all file types
 			},
